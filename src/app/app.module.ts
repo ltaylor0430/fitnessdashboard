44f8ffa328373c2e9bbe9bc76fee586bb466fa91
Lawrence Taylor <lawrence.taylor@silvervinesoftware.com>
2018-04-18 16:59:40 -0400

    Not implemented, just going to leave this here.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module.';
 import { WorkoutModule } from './workout/workout.module';
 import { SignInModule } from './signin/signin.module';
-
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { ApolloLink, concat } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import {environment } from './../environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    HttpLinkModule,
+    ApolloModule,
     BrowserModule,
     SharedModule,
     SignInModule,
@@ -20,4 +28,26 @@ import { SignInModule } from './signin/signin.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    const http = httpLink.create({ uri: environment.graphQL });
+    const authMiddleware = new ApolloLink((operation, forward) => {
+      // add the authorization to the headers
+      operation.setContext({
+        headers: new HttpHeaders().set('Authorization', 'Bearer:' + localStorage.getItem('token') || null)
+      });
+
+      return forward(operation);
+    });
+
+    apollo.create({
+      link: concat(authMiddleware, http),
+      cache:  new InMemoryCache()
+    });
+
+
+  }
+}
--- src/app/workout/workout.module.ts
+++ src/app/workout/workout.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NewRoutineComponent } from './new-routine/new-routine.component';
 import { EditRoutineComponent } from './edit-routine/edit-routine.component';
 
+
+
 @NgModule({
   imports: [
     CommonModule
